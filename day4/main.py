c53151475fa84577b4fc84ff929f9c97d04bda9b
kasparas-ban <kaspis245@gmail.com>
2022-12-16 19:32:22 +0200

    Day 4

---
+++ day4/main.py
@@ -0,0 +1,16 @@
+def checkContains(sec1, sec2):
+    [min1, max1] = sec1.split('-')
+    [min2, max2] = sec2.split('-')
+    if (int(min1) >= int(min2) and int(max1) <= int(max2)) or (int(min2) >= int(min1) and int(max2) <= int(max1)):
+        return 1
+    return 0
+
+total = 0
+
+with open('input.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        [sec1, sec2] = line.split(',')
+        total += checkContains(sec1, sec2)
+
+print(total)
